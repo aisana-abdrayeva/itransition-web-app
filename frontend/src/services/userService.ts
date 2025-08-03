f0543794407931f45a72deba9d3f9179c6feb6c7
Aisana Abdrayeva <aisana.abdrayeva@gmail.com>
2025-08-03 21:24:14 +0500

    fix get users data

--- frontend/src/services/userService.ts
+++ frontend/src/services/userService.ts
@@ -1,21 +1,21 @@
 import api from "../api";
 
 export const getUsers = async () => {
-    const res = await api.get("/users");
+    const res = await api.get("/api/users");
     return res.data;
 }
 
 export const blockUser = async (userId: string) => {
-    const res = await api.post(`/users/${userId}/block`);
+    const res = await api.post(`/api/users/${userId}/block`);
     return res.data;
 }
 
 export const unblockUser = async (userId: string) => {
-    const res = await api.post(`/users/${userId}/unblock`);
+    const res = await api.post(`/api/users/${userId}/unblock`);
     return res.data;
 }
 
 export const deleteUser = async (userId: string) => {
-    const res = await api.delete(`/users/${userId}`);
+    const res = await api.delete(`/api/users/${userId}`);
     return res.data;
 }
