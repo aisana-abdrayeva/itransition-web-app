b89eed01d29a9a904f9679024f35c5476adcbcdb
Aisana Abdrayeva <aisana.abdrayeva@gmail.com>
2025-08-04 22:02:42 +0500

    lil fix

--- backend/src/middleware/authGuard.ts
+++ backend/src/middleware/authGuard.ts
@@ -5,8 +5,7 @@ function authGuard(req: any, res: any, next: any) {
     if (!token) return res.status(401).json({ error: "Not authenticated" });
 
     try {
-        const user = jwt.verify(token, process.env.JWT_SECRET);
-        res.json({ message: "Access granted", user });
+        req.user = jwt.verify(token, process.env.JWT_SECRET);
         next();
     } catch (err) {
         return res.status(401).json({ error: "Invalid or expired token" });
